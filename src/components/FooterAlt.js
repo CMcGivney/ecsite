df0d5db42fd9d34e95bdd9b89fe49624734e42dd
Chris McGivney <cmmcgivney84@gmail.com>
2020-06-23 15:36:34 -0600

    chore: added copyright protection on the footer

--- src/components/FooterAlt.js
+++ src/components/FooterAlt.js
@@ -7,14 +7,24 @@ class Footer extends React.Component {
  
   render() {
     return (
-        <>
-        <div className="footerGrid">
-          <Header.Content className='mainFooter'>Elysian Catering</Header.Content>
-           <Header.Content><SocialFollow/></Header.Content>
-            <Header.Content className='phoneNum'>
-              <Icon name="phone"/><a className="email" href="tel:+1-385-368-3525">1-(385)-368-3525</a>
+      
+        <div className="footerFlex">
+       
+           <Header.Content as="h1" className='mainFooter'>
+             Elysian Catering
+           </Header.Content>
+         <div>
+           <Header.Content as="h4" className='phoneNum'>
+              <Icon name="phone"/>
+               <a 
+                className="email" 
+                href="tel:+1-385-368-3525">
+                1-(385)-368-3525
+               </a>
             </Header.Content>
-             <Header.Content className='emailBox'>
+           </div>
+           <div>
+             <Header.Content as="h4">
                <Icon name="mail"/>
                <a 
                  className="email" 
@@ -24,19 +34,35 @@ class Footer extends React.Component {
                  AsuntaGaribay18@gmail.com
                </a>
              </Header.Content>
-          {/* <div className='bizAddress'>
-            <Header.Content>
-            business address here 
+          </div>
+          <div className="bizAddress">
+            <Header.Content as="h4">
+             <Icon name="map pin"/>
+              <a 
+                className="email"  
+                target="_blank" 
+                rel="noopener noreferrer" 
+                href="https://www.google.com/maps/place/640+Wilmington+Ave,+Salt+Lake+City,+UT+84106/@40.7229828,-111.8749303,17z/data=!3m1!4b1!4m5!3m4!1s0x87528abb89b1543f:0x6b967e62340c1c8b!8m2!3d40.7229828!4d-111.8727416"
+                > 640 East Wilmington Ave, Salt Lake City, UT 84106 
+              </a>
             </Header.Content>
-           </div> */}
-         
+          </div>
+          
+           <Header.Content as="h4">
+             <SocialFollow/>
+           </Header.Content>
+           <div>
+            <Header.Content as="h6">
+             © 2019-2020 The Elysian Catering, all rights reserved.
+            </Header.Content>
+           </div>
           {/* <div className="poweredBy">
             <Header.Content>
               Powered by: <a className="email" href="https://chris-portfo.herokuapp.com/">84Creations</a>
              </Header.Content>
           </div> */}
         </div>
-       </>
+       
      )
     }
   }
