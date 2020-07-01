0f5e26bd49aa6792458c3c0927f694912bdfd723
Chris McGivney <cmmcgivney84@gmail.com>
2020-07-01 09:37:56 -0600

    update: Client send src for vendor badge

--- src/components/FindUs.js
+++ src/components/FindUs.js
@@ -1,6 +1,5 @@
 import React from "react"
 import {Image} from "semantic-ui-react"
-import TheKnot from "../Images/theKnot.jpeg"
 import WeddingWire from "../Images/weddingWire.png"
 
 
@@ -10,9 +9,10 @@ const FindUs = () => (
     
       <Image
        className="findUsItem"
-       src={TheKnot}
-       alt="The Knot Logo, wedding planning site"
-       href="https://www.theknot.com/marketplace/elysian-catering-salt-lake-city-ut-2038693"
+       src="https://www.xoedge.com/myaccount/2019/website-share/VendorBadge_AsSeenOnWeb.png"
+       alt="As Seen on The Knot"
+       size="small"
+       href="https://www.theknot.com/marketplace/redirect-2038693?utm_source=vendor_website&utm_medium=banner&utm_term=68b97cb8-4426-42a9-a6eb-48aef0035bbb&utm_campaign=vendor_badge_assets"
        target="_blank"
        rel="noopener noreferrer"
        />
@@ -20,6 +20,7 @@ const FindUs = () => (
        className="findUsItem"
        src={WeddingWire}
        alt="Wedding Wire logo, wedding planning site"
+       size="small"
        href="https://www.weddingwire.com/biz/elysian-catering/4e43cba1dbf993d6.html"
        target="_blank"
        rel="noopener noreferrer"
@@ -28,5 +29,13 @@ const FindUs = () => (
     </div>
 
 )
+{/* <a target="_blank" 
+href="https://www.theknot.com/marketplace/redirect-2038693?utm_source=vendor_website&utm_medium=banner&utm_term=68b97cb8-4426-42a9-a6eb-48aef0035bbb&utm_campaign=vendor_badge_assets">
+<img 
+alt="As Seen on The Knot"
+ border="0"
+ src="https://www.xoedge.com/myaccount/2019/website-share/VendorBadge_AsSeenOnWeb.png" 
+ width="190">
 
+</a> */}
 export default FindUs;
